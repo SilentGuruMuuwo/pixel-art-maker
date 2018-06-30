ca302297bb96981c8d728bc547423c369a608d15
SilentGuruMuuwo <32681716+SilentGuruMuuwo@users.noreply.github.com>
2018-07-01 02:18:36 +0300

    Update designs.js
    changed code to allow for color selection into the grid cells

--- designs.js
+++ designs.js
@@ -1,33 +1,27 @@
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
+//declaring the height, width and color variables
 var height, width, color;
 $('#sizePicker').on('submit', function(event){
 	event.preventDefault();
 	height = $('#inputHeight').val();
 	width = $('#inputWidth').val();
+	color = $('#colorPicker').val();
+//we call the makegrid function below
 	makeGrid(height, width);
 });
 
 function makeGrid(h,w) {
+//creating the makegrid function which allows us to input height and width values for the grid
 	$('tr').remove();
 	for(var i = 1; i<=h; i++){
 		$('#pixelCanvas').append('<tr id=table' + i + '></tr>');
 		for(var j = 1; j<=w; j++){
-			$('tr').filter(':last').append('<td></td>')
+			$('tr').filter(':last').append('<td></td>');
+			$('td').attr("class", 'cells');
 		}
 	};
-	$('td').on('click', function addColor(){
-		color = $('#colorPicker').val();
-
-		if($(this).attr('style')){
-			$(this).removeAttr('style');
-		}
-		else {
-			$(this).attr('style','background-color' + color);
-		}
-
+	//this section of the code allows you to allocate colors to the cells in the grid created
+	$('.cells').click(function (event) {
+  		var paint = $('#colorPicker').val();
+  		$(event.target).css('background-color', paint);
 	});
-
 };
